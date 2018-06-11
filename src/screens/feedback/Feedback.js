0fe149fbd92c4119cfb9ec6fda76e977ba352b1f
omotosho ayobami <ayobamiomotosho@yahoo.com>
2018-06-11 11:57:50 +0100

    feedback update

--- src/screens/feedback/Feedback.js
+++ src/screens/feedback/Feedback.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import { Button, FormInput, FormLabel } from "react-native-elements";
+import { Button, FormInput, FormLabel, FormValidationMessage } from "react-native-elements";
 
 import AppTitleHeader from '../../common/AppTitleHeader';
 
@@ -9,11 +9,64 @@ class Feedback extends Component {
 
     constructor(props) {
         super(props);       
+
+        this.state = {
+            topic:'',
+            topicError:'',
+            message:'',
+            messageError: ''
+        }
     }
 
+    onTopicChange = (text) =>{
+        if (text.trim() == '') {
+            this.setState({topicError: "topic can not be blank."});
+        }else{
+            this.setState({topicError: ''});
+        }
+
+        this.setState({topic: text});
+    }
+
+    onMessageChange = (text) =>{
+        if (text.trim() == '') {
+            this.setState({ messageError: "message can not be blank." });
+        } else {
+            this.setState({ messageError: '' });
+        }
+
+        this.setState({ message: text });
+    }
 
     onSubmitPress = () =>{
-        alert("thanks");
+        const { topic, topicError, message, messageError } = this.state;
+        let error = 0;
+
+        if(topic.trim() == ''){
+            this.setState({ topicError: 'topic can not be blank.'})
+            error++;
+        }
+
+        if (message.trim() == '') {
+            this.setState({ messageError: 'message can not be blank.' })
+            error++;
+        }
+
+        if (error <= 0) {
+            this.props.navigator.showInAppNotification({
+                screen: 'nbaApp.ErrorNotification',
+                passProps: {
+                    message: 'pls fill all fields.'
+                },
+            })
+        }else{
+            this.props.navigator.showInAppNotification({
+                screen: 'nbaApp.SuccessNotification',
+                passProps: {
+                    message: 'feedback submitted successfully.'
+                },
+            })
+        }
     }
 
     render() {
@@ -29,17 +82,24 @@ class Feedback extends Component {
                         <FormLabel>TOPIC</FormLabel>
                         <FormInput 
                             placeholder = 'topic goes here'
+                            underlineColorAndroid='#91B38D'
+                            onChangeText={(text => { this.onTopicChange(text) })}
                         />
+                        <FormValidationMessage>{this.state.topicError}</FormValidationMessage>
 
                         <FormLabel>MESSAGE</FormLabel>
                         <FormInput
                             placeholder='message goes here'
+                            underlineColorAndroid='#91B38D'
                             multiline = {true}
+                            onChangeText={(text => { this.onMessageChange(text) })}
                         />
+                        <FormValidationMessage>{this.state.messageError}</FormValidationMessage>
 
                         <Button
                             title = "SUBMIT"
-                            style = {styles.submitButton}
+                            fontWeight='bold'
+                            buttonStyle = {styles.submitButton}
                             onPress ={() => {this.onSubmitPress()}}
                             />
                     </View>
@@ -59,6 +119,8 @@ const styles = StyleSheet.create({
 
     },
     submitButton:{
-        marginTop: 10
+        marginTop: 10,
+        backgroundColor: '#036735'
+
     }
 })
